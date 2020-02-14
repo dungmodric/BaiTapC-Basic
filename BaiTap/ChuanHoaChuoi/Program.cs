71edd789944ab2832915bfe6963fd46cd3cde917
= <=>
2020-02-14 22:07:43 +0700

    14-02-2020

---
+++ BaiTap/ChuanHoaChuoi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChuanHoaChuoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BÀI 2: CHUẨN HÓA CHUỖI ------------------------------------------------------------
+            Console.Write("Nhap chuoi: ");
+            string n = Console.ReadLine();
+            n = n.Trim().ToLower();
+            while (n.Contains("  "))
+                n = n.Replace("  ", " ");
+            string ketqua = "";
+            string[] a = n.Split(' ');
+            foreach (string i in a)
+            {
+                ketqua += i.Substring(0, 1).ToUpper() + i.Substring(1) + " ";
+            }
+            Console.Write("Chuoi duoc chuan hoa: " + ketqua.TrimEnd()) ;
+            Console.ReadKey();
+        }
+    }
+}
---
+++ BaiTap/Chuyendoihecoso/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chuyendoihecoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BÀI 1:
+            //CHUYỂN CƠ SỐ 10 SANG CƠ SỐ 2 ------------------------------------------------------
+            //int n, i;
+            //int[] a = new int[100];
+            //Console.Write("Nhap so can chuyen: ");
+            //n = int.Parse(Console.ReadLine());
+            //for (i = 0; n > 0; i++)
+            //{
+            //    a[i] = n % 2;
+            //    n = n / 2;
+            //}
+            //Console.Write("Co so 2 la: ");
+            //for (i = i - 1; i >= 0; i--)
+            //{
+            //    Console.Write(a[i]);
+            //}
+            //Console.ReadKey();
+
+
+            //CHUYỂN CƠ SỐ 16 SANG CƠ SỐ 2 ------------------------------------------------------
+            //string cs2 = "";
+            //Console.Write("Nhap so can chuyen: ");
+            //string n = Console.ReadLine();
+            //Console.Write("Co so 2 la: ");
+            //for (int i = 0; i < n.Length; i++)
+            //{
+            //    string newN = n.Substring(i, 1);
+            //    switch (newN)
+            //    {
+            //        case "0":
+            //            cs2 += "0000"; break;
+            //        case "1":
+            //            cs2 += "0001"; break;
+            //        case "2":
+            //            cs2 += "0010"; break;
+            //        case "3":
+            //            cs2 += "0011"; break;
+            //        case "4":
+            //            cs2 += "0100"; break;
+            //        case "5":
+            //            cs2 += "0101"; break;
+            //        case "6":
+            //            cs2 += "0110"; break;
+            //        case "7":
+            //            cs2 += "0111"; break;
+            //        case "8":
+            //            cs2 += "1000"; break;
+            //        case "9":
+            //            cs2 += "1001"; break;
+            //        case "A":
+            //            cs2 += "1010"; break;
+            //        case "B":
+            //            cs2 += "1011"; break;
+            //        case "C":
+            //            cs2 += "1100"; break;
+            //        case "D":
+            //            cs2 += "1101"; break;
+            //        case "E":
+            //            cs2 += "1110"; break;
+            //        case "F":
+            //            cs2 += "1111"; break;
+            //        default:
+            //            cs2 += "";
+            //            break;
+            //    }
+            //}
+            //Console.Write(cs2);
+            //Console.ReadKey();
+
+
+            // CHUYỂN CƠ SỐ 8 SANG CƠ SỐ 2 ------------------------------------------------
+            //string cs2 = "";
+            //Console.Write("Nhap so can chuyen: ");
+            //string n = Console.ReadLine();
+            //Console.Write("Co so 2 la: ");
+            //for (int i = 0; i < n.Length; i++)
+            //{
+            //    string newN = n.Substring(i, 1);
+            //    switch (newN)
+            //    {
+            //        case "0":
+            //            cs2 += "000"; break;
+            //        case "1":
+            //            cs2 += "001"; break;
+            //        case "2":
+            //            cs2 += "010"; break;
+            //        case "3":
+            //            cs2 += "011"; break;
+            //        case "4":
+            //            cs2 += "100"; break;
+            //        case "5":
+            //            cs2 += "101"; break;
+            //        case "6":
+            //            cs2 += "110"; break;
+            //        case "7":
+            //            cs2 += "111"; break;
+            //        default:
+            //            cs2 += "";
+            //            break;
+            //    }
+            //}
+            //Console.Write(cs2);
+            //Console.ReadKey();
+        }
+    }
+}
