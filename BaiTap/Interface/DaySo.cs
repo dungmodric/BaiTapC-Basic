f98f0de45bbdb05b5678c08165e47b70376b255f
= <=>
2020-02-16 21:43:36 +0700

    16-02

---
+++ BaiTap/Interface/DaySo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class DaySo
+    {
+        int i;
+        public void Nhap() 
+        {
+            Console.Write("Nhap phan tu thu {0}: ", i);
+            Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Print(int[] A)
+        {
+            foreach (var a in A)
+            {
+                Console.Write(a);
+            }
+        }
+
+        public void DaySo1(int n)
+        {
+            int[] A = new int[n];
+            for (i = 1; i <= n; i++)
+            {
+                Nhap();
+            }
+        }
+    }
+}
---
+++ BaiTap/Interface/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so phan tu cua mang A: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            DaySo ds = new DaySo();
+            ds.DaySo1(n);
+            Console.ReadKey();
+        }
+    }
+}
